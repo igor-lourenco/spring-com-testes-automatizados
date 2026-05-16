695f2c418da8e7cefd6c94d0aa810e8bc8b4d1a6
igor-lourenco <igor.lourencosantos@gmail.com>
2026-05-16 13:21:37 -0300

    Adicionando os campos: createdAt e updatedAt em Category, como dados de auditoria

--- src/main/java/com/expert/testes/entities/Category.java
+++ src/main/java/com/expert/testes/entities/Category.java
@@ -1,10 +1,11 @@
 package com.expert.testes.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Data
@@ -20,5 +21,20 @@ public class Category {
     private Long id;
     private String name;
 
+    @Setter(AccessLevel.NONE)
+    @CreationTimestamp // serve para preencher automaticamente um campo com a data e hora de criação da entidade, no momento em que ela é persistida pela primeira vez no banco de dados.
+    @Column(name = "created_at", length = 6,
+        nullable = false,
+        updatable = false // para não atualizar no banco de dados após criado
+    )
+    private LocalDateTime createdAt;
+
+
+    @Setter(AccessLevel.NONE)
+    @UpdateTimestamp // serve para atualizar automaticamente um campo com a data/hora da última modificação da entidade, sempre que um UPDATE acontece no banco.
+    @Column(name = "updated_at",
+        insertable = false // para não ser criado no banco de dados, ou seja, salvar como null
+    )
+    private LocalDateTime updatedAt;
 }
 
