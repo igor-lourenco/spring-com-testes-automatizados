e93eedd6ee32798e4db478500ff6a2a6376d53ba
igor-lourenco <igor.lourencosantos@gmail.com>
2026-05-22 15:16:26 -0300

    Implementando Fixtures @BeforeEach: preraração antes de cada teste da classe

--- src/test/java/com/expert/testes/repositories/ProductRepositoryTests.java
+++ src/test/java/com/expert/testes/repositories/ProductRepositoryTests.java
@@ -2,6 +2,7 @@ package com.expert.testes.repositories;
 
 import com.expert.testes.entities.Product;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
@@ -11,8 +12,16 @@ import java.util.Optional;
 @DataJpaTest // Carrega somente os componentes relacionados ao Spring Data JPA. Cada teste é transacional e dá rollback ao final. (teste de unidade: repository)
 public class ProductRepositoryTests {
 
+    private long existingId;
+
     @Autowired
     private ProductRepository repository; // considera o seed do banco que tiver no arquivo import.sql
+
+
+    @BeforeEach // Preparação antes de cada teste da classe
+    void setUp(){
+        existingId = 1L;
+    }
 
 
 //	Nomenclatura de um teste: <AÇÃO> should <EFEITO> [when <CENÁRIO>]
@@ -22,7 +31,7 @@ public class ProductRepositoryTests {
 //      Padrão AAA
 
 //   	Arrange: instancie os objetos necessários
-        long existingId = 1L;
+//      long existingId = 1L;
 
 //      Act: execute as ações necessárias
         repository.deleteById(existingId);
@@ -31,4 +40,6 @@ public class ProductRepositoryTests {
         Optional<Product> result = repository.findById(existingId);
         Assertions.assertFalse(result.isPresent());
     }
+
+
 }
