e0bbbc7b7aa16bed6ab82f7c495a6659513d6fbf
igor-lourenco <igor.lourencosantos@gmail.com>
2026-06-07 15:59:33 -0300

    Implementando CRUD na camada controller para User, e liberando o banco H2 no Spring Security

--- src/main/java/com/expert/testes/configs/SecurityConfig.java
+++ src/main/java/com/expert/testes/configs/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,9 +17,17 @@ public class SecurityConfig {
 
 //      TODO: Por enquanto todas as APIs estão sendo liberadas
         http
-            .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
-            .csrf(AbstractHttpConfigurer::disable)
-            .cors(AbstractHttpConfigurer::disable); // Desativa proteção contra CSRF (Cross-Site Request Forgery) porque o ataque de CSRF geralmente depende de um navegador do usuário e de cookies de autenticação
+            .authorizeHttpRequests(auth -> auth // Libera tudo
+                .requestMatchers("/h2-console/**").permitAll()
+                .anyRequest().permitAll()
+            )
+            .csrf(csrf -> csrf // Desativa proteção contra CSRF (Cross-Site Request Forgery) porque o ataque de CSRF geralmente depende de um navegador do usuário e de cookies de autenticação
+                .ignoringRequestMatchers("/h2-console/**") // Desabilita CSRF para testes (necessário pro H2 funcionar)
+                .disable()
+            )
+            .headers(headers -> headers // Para renderizar H2 no browser (iframe)
+                .frameOptions(frame -> frame.disable())
+            );
 
         return http.build();
     }
---
+++ src/main/java/com/expert/testes/controllers/UserController.java
@@ -0,0 +1,100 @@
+package com.expert.testes.controllers;
+
+import com.expert.testes.DTOs.UserDTO;
+import com.expert.testes.DTOs.UserWithPasswordDTO;
+import com.expert.testes.services.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@Log4j2
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(path = "/v1/users")
+public class UserController {
+
+    private final UserService service;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserDTO> findAll() {
+        log.info("REQUEST - GET [findAll]");
+
+        List<UserDTO> userDTOs = service.findAll();
+
+        log.info("RESPONSE - GET [findAll]");
+        return userDTOs;
+    }
+
+
+    @GetMapping(value = "/page")
+    @ResponseStatus(HttpStatus.OK)
+    public Page<UserDTO> findAllPaged(
+        @PageableDefault(page = 0, size = 12, sort = "name", direction = Sort.Direction.ASC) Pageable pageable) {
+        log.info("REQUEST - GET [findAllPaged]");
+
+        Page<UserDTO> userDTOs = service.findAllPaged(pageable);
+
+        log.info("RESPONSE - GET [findAllPaged]");
+        return userDTOs;
+    }
+
+
+    @GetMapping(value = "{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserDTO findById(@PathVariable Long id) {
+        log.info("REQUEST - GET [findById]");
+
+        UserDTO userDTO = service.findById(id);
+
+        log.info("RESPONSE - GET [findById]");
+        return userDTO;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<UserDTO> insert(@RequestBody UserWithPasswordDTO userWithPasswordDTO) {
+        log.info("REQUEST - POST [insert]");
+
+        UserDTO userDTO = service.insert(userWithPasswordDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(userDTO.id())
+            .toUri();
+
+        log.info("RESPONSE - POST [insert]");
+        return ResponseEntity.created(uri).body(userDTO);
+    }
+
+
+    @PutMapping(value = "{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserDTO update(@PathVariable Long id, @RequestBody UserDTO dto) {
+        log.info("REQUEST - PUT [update]");
+
+        UserDTO userDTO = service.update(id, dto);
+
+        log.info("RESPONSE - PUT [update]");
+        return userDTO;
+    }
+
+    @DeleteMapping(value = "{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        log.info("REQUEST - DELETE [delete]");
+
+        service.delete(id);
+
+        log.info("RESPONSE - DELETE [delete]");
+    }
+}
