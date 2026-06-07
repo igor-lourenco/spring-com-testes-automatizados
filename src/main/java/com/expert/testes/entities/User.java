e40ee922831afd137cfa4143c91d12576aba5547
igor-lourenco <igor.lourencosantos@gmail.com>
2026-06-07 13:38:24 -0300

    Mapeando objeto-relacional apenas no User, e seed do banco

--- src/main/java/com/expert/testes/entities/User.java
+++ src/main/java/com/expert/testes/entities/User.java
@@ -26,6 +26,22 @@ public class User {
     private String password;
 
     @Setter(AccessLevel.NONE)
+    @ManyToMany(fetch = FetchType.EAGER, // por padrão usa o Fetch.LAZY
+        cascade = { CascadeType.PERSIST  // ao persistir produto, também irá salvar as categorias em cascata
+            , CascadeType.MERGE} ) // ao atualizar produto, também irá salvar as categorias em cascata
+    @JoinTable(name = "tb_user_role",
+//        foreignKey = @ForeignKey(name = "fk_product_category_product"), // exemplo de configurar o nome da contraint diretamente no @JoinTable em vez do @JoinColumn
+//        inverseForeignKey = @ForeignKey(name = "fk_product_category_category") // exemplo de configurar o nome da contraint diretamente no @JoinTable em vez do @JoinColumn
+
+        joinColumns = @JoinColumn(name = "user_id", // coluna que referencia o id dessa entidade Produto (owner)
+            nullable = false,
+            foreignKey = @ForeignKey(name = "fk_user_role_user") // nome da constraint de chave estrangeira
+        ),
+        inverseJoinColumns = @JoinColumn(name = "role_id", // coluna que referencia o id da entidade Categoria (não owner)
+            nullable = false,
+            foreignKey = @ForeignKey(name = "fk_user_role_role") // nome da constraint de chave estrangeira
+        )
+    )
     @Builder.Default // Para o @Builder respeitar o valor default e evitar o NullPointerException
     private Set<Role> roles = new HashSet<>();
 
