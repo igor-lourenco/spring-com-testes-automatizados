2cc48628532eb02ca2505c22a5e8430648385096
igor-lourenco <igor.lourencosantos@gmail.com>
2026-07-07 11:11:17 -0300

    Teste unitário (isolado na camada Service) usando @ExtendWith - sendEmailDeveEnviarEmailComSucesso

---
+++ src/test/java/com/expert/testes/services/EmailServiceTests.java
@@ -0,0 +1,67 @@
+package com.expert.testes.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.util.ReflectionTestUtils;
+
+//@ExtendWith(SpringExtension.class) // Não carrega o contexto, mas permite usar os recursos do Spring com JUnit (teste de unidade: service/component)
+@ExtendWith(MockitoExtension.class) // Não carrega o contexto, mas permite usar os recursos do Spring com JUnit (teste de unidade: service/component)
+public class EmailServiceTests {
+
+    @InjectMocks // Define o objeto principal que está sendo testado, cria uma instância real dessa classe e injeta automaticamente todos os mocks criados nela
+    private EmailService emailService;
+
+    @Mock // Cria uma simulação, evita conexões reais com o banco de dados e permite programar retornos fictícios para os métodos do repositório.
+    private JavaMailSender emailSender;
+
+    private String to, subject, body;
+
+    @BeforeEach
+    void setUp() {
+        to = "destinatario@teste.com";
+        subject = "Assunto de teste";
+        body = "Corpo do e-mail";
+
+//      Como o teste unitário não sobe o contexto Spring inteiro, o campo 'emailFrom' não é preenchido pelo @Value
+        ReflectionTestUtils.setField(
+            emailService,
+            "emailFrom",
+            "noreply@teste.com"
+        );
+    }
+
+
+    @Test //  <sendEmail> deve <EnviarEmailComSucesso> [quando <>]
+    public void sendEmailShouldSendEmailSuccessfully() {
+//      -> Padrão AAA
+
+//   	-> Arrange: instancie os objetos necessários
+        ArgumentCaptor<SimpleMailMessage> captor =  // serve para capturar o argumento que foi passado para um método mockado (ainda não tem nada dentro)
+            ArgumentCaptor.forClass(SimpleMailMessage.class);
+
+
+//      -> Act: execute as ações necessárias
+        emailService.sendEmail(to, subject, body);
+
+
+//      -> Assert: declare o que deveria acontecer (resultado esperado)
+        Mockito.verify(emailSender) //  garante que o método do 'emailSender.send' que está dentro do 'emailService.sendEmail' foi chamado
+            .send(captor.capture()); // captura o objeto SimpleMailMessage que foi passado para o método send;
+
+        SimpleMailMessage message = captor.getValue(); // representa exatamente o SimpleMailMessage que o service tentou enviar.
+
+        Assertions.assertEquals("noreply@teste.com", message.getFrom());
+        Assertions.assertEquals(to, message.getTo()[0]);
+        Assertions.assertEquals(subject, message.getSubject());
+        Assertions.assertEquals(body, message.getText());
+    }
+}
